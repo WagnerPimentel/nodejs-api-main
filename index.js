6a91f147c83f8828f99b4cbc89559435bafd6123
Wagner Pimentel <142364404+WagnerPimentel@users.noreply.github.com>
2024-06-25 21:28:11 -0300

    atualizaçao

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get("/heroes", function(req, res){
     res.send(herois)
 })
 
+
+//ENDPOINT listagem de UM herói (READ SINGLE => [GET])
 app.get("/heroes/:id", function(req, res){
     //acessar o parametro de rota ID
     const id = req.params.id
@@ -17,6 +19,15 @@ app.get("/heroes/:id", function(req, res){
 
     //envio o item encontrado como resposta
     res.send(umheroi)
+})
+
+//todo o corpo da requisição virá como JSON
+app.use(express.json())
+
+
+//ENDPOINT de criação (CREATE => [POST])
+app.post("/heroes", function(req, res){
+    res.send(req.body)
 })
 
 app.listen(3000)
